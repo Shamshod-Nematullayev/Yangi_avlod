aefe6649459186051942f570c39ec8439ff2dedf
Shamshod-Nematullayev <100130334+Shamshod-Nematullayev@users.noreply.github.com>
2022-05-25 16:37:43 +0500

    g
    df

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,11 +1,20 @@
-const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const db = mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(()=>{
-        console.log('MongoDB connected...');
-    })
-    .catch(err=>{
-        console.log('MongoDB connection error...', err);
-    })
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// const db = mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(()=>{
+//         console.log('MongoDB connected...');
+//     })
+//     .catch(err=>{
+//         console.log('MongoDB connection error...', err);
+//     })
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 module.exports = db;
